c99457408ce355d19fea77034a95c23c417e3198
Logan Caraco <lbc.dna@gmail.com>
2021-09-03 20:09:35 -0700

    changed sublist

--- questions/sublist/source.py
+++ questions/sublist/source.py
@@ -2,7 +2,7 @@
 
 def is_sublist(list, sublist): #0given
     n, m = len(list), len(sublist)
-    for i in range(?n - m?): #blank _ - _
+    for i in range(?n - m?):
         start, end = i, i + m
         if list[?start:end?] == sublist:
             return ?True?
--- questions/sublist/tests/ans.py
+++ questions/sublist/tests/ans.py
@@ -2,7 +2,7 @@
 
 def is_sublist(list, sublist): 
     n, m = len(list), len(sublist)
-    for i in range(n - m): 
+    for i in range(n - m):
         start, end = i, i + m
         if list[start:end] == sublist:
             return True
