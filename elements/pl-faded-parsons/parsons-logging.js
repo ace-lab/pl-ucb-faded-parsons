83efa424b5fbbb95dafde35e7c787f5b5932d9ad
Logan Caraco <lbc.dna@gmail.com>
2021-09-09 16:40:16 -0700

    wrote typing event logic

--- elements/pl-faded-parsons/parsons-logging.js
+++ elements/pl-faded-parsons/parsons-logging.js
@@ -9,35 +9,69 @@
         console.log(event);
     };
 
-    ParsonsLogger.prototype.onBlankUpdate = function(event, codeline) {
-        let start, time;
-        start = time = Date.now();
+    ParsonsLogger.prototype.finishTypingEvent = function() {
+        if (!this.last_field_update) return;
+        
+        clearTimeout(this.last_field_update.timeout);
 
-        if (this.last_field_update) {
-            clearTimeout(this.last_field_update.timeout);
-            start = this.last_field_update.start;
-        }
-        
-        this.last_field_update = { 
-            id: codeline.id, 
-            e: event, 
-            start: start,
-            time: time,
-            value: codeline.value,
-            timeout: setTimeout(() => {
-                const e = {
-                    type: 'typed',
-                    time: this.last_field_update.start,
-                    duration: this.last_field_update.time - this.last_field_update.start,
-                    codelineId: this.last_field_update.id,
-                    codelineValue: this.last_field_update.value,
-                };
-        
-                console.log(this, e);
-                this.events.push(e);
-        
-                this.last_field_update = null;
-            }, 500),
+        const e = {
+            type: 'typed',
+            time: this.last_field_update.start,
+            duration: this.last_field_update.time - this.last_field_update.start,
+            codelineId: this.last_field_update.id,
+            codelineValue: this.last_field_update.value,
         };
+
+        console.log('typing event', e);
+        this.events.push(e);
+
+        this.last_field_update = null;
+    }
+
+    ParsonsLogger.prototype.onBlankUpdate = function(event, codeline) {
+        let time = Date.now();
+
+        switch (event.inputType) {
+            case 'insertFromPaste': 
+            case 'insertFromPasteAsQuotation':
+            case 'insertFromDrop':
+                this.finishTypingEvent();
+
+                const e = {
+                    type: 'paste',
+                    time: time,
+                    duration: 0,
+                    codelineId: codeline.id,
+                    codelineValue: codeline.value,
+                };
+
+                console.log('paste event', e);
+                this.events.push(e);
+                break;
+            case 'deleteByDrag':
+            case 'deleteByCut':
+                // do something special?
+                // right now just continues to default...
+            default: // generic typing event
+                if (this.last_field_update && this.last_field_update.id !== codeline.name) {
+                    this.finishTypingEvent();
+                }
+                
+                let start = time;
+        
+                if (this.last_field_update) {
+                    clearTimeout(this.last_field_update.timeout);
+                    start = this.last_field_update.start;
+                }
+                
+                this.last_field_update = { 
+                    id: codeline.name, 
+                    e: event, 
+                    start: start,
+                    time: time,
+                    value: codeline.value,
+                    timeout: setTimeout(() => this.finishTypingEvent(), 1000),
+                };
+        }
     }
 })();
