6902853f2b779dba10bc967b58bc39d20f2d508d
Logan Caraco <lbc.dna@gmail.com>
2021-09-10 10:52:39 -0700

    added onSubmit, refactoring

--- elements/pl-faded-parsons/parsons-logging.js
+++ elements/pl-faded-parsons/parsons-logging.js
@@ -1,9 +1,24 @@
 (function() {
-    var ParsonsLogger = function() {
-        this.events = new Array();
+    var ParsonsLogger = function(widget) {
+        this.widget = widget;
+        this.events = [];
         this.last_field_update = null;
     };
+
     window['ParsonsLogger'] = ParsonsLogger;
+
+    ParsonsLogger.prototype.logEvent = function(e) {
+        e['widgetId'] = this.widget.options.sortableId;
+        e['time'] = e['time'] || Date.now();
+        this.events.push(e);
+    };
+
+    ParsonsLogger.prototype.onSubmit = function() {
+        const e = { type: 'submit' };
+
+        console.log('submit event', e);
+        this.logEvent(e);
+    };
 
     ParsonsLogger.prototype.onSortableUpdate = function(event, ui) {
         console.log(event);
@@ -17,20 +32,18 @@
         const e = {
             type: 'typed',
             time: this.last_field_update.start,
-            duration: this.last_field_update.time - this.last_field_update.start,
-            codelineId: this.last_field_update.id,
+            duration: Date.now() - this.last_field_update.start,
+            codelineName: this.last_field_update.codelineName,
             codelineValue: this.last_field_update.value,
         };
 
         console.log('typing event', e);
-        this.events.push(e);
+        this.logEvent(e);
 
         this.last_field_update = null;
     }
 
     ParsonsLogger.prototype.onBlankUpdate = function(event, codeline) {
-        let time = Date.now();
-
         switch (event.inputType) {
             case 'insertFromPaste': 
             case 'insertFromPasteAsQuotation':
@@ -39,36 +52,34 @@
 
                 const e = {
                     type: 'paste',
-                    time: time,
                     duration: 0,
-                    codelineId: codeline.id,
+                    codelineName: codeline.name,
                     codelineValue: codeline.value,
                 };
 
                 console.log('paste event', e);
-                this.events.push(e);
+                this.logEvent(e);
                 break;
             case 'deleteByDrag':
             case 'deleteByCut':
                 // do something special?
                 // right now just continues to default...
             default: // generic typing event
-                if (this.last_field_update && this.last_field_update.id !== codeline.name) {
-                    this.finishTypingEvent();
-                }
-                
-                let start = time;
-        
                 if (this.last_field_update) {
-                    clearTimeout(this.last_field_update.timeout);
-                    start = this.last_field_update.start;
+                    // if the last update was to a different field, finish the previous event
+                    // otherwise clear the timeout
+                    if (this.last_field_update.codelineName !== codeline.name) {
+                        this.finishTypingEvent();
+                    } else {
+                        clearTimeout(this.last_field_update.timeout);
+                    }
                 }
                 
                 this.last_field_update = { 
-                    id: codeline.name, 
+                    codelineName: codeline.name, 
                     e: event, 
-                    start: start,
-                    time: time,
+                    start: this.last_field_update ? 
+                        this.last_field_update.start : Date.now(),
                     value: codeline.value,
                     timeout: setTimeout(() => this.finishTypingEvent(), 1000),
                 };
