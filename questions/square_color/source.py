676d2b3c6827c0bc124c22647304d5292dea1368
Logan Caraco <lbc.dna@gmail.com>
2021-09-15 15:24:55 -0700

    finished square_color, reran tool

--- questions/square_color/source.py
+++ questions/square_color/source.py
@@ -1,23 +1,27 @@
-""" Make a function <code>is_sublist</code> that checks wether the first
-    argument contains the second as a sublist (including ordering), eg
+""" Make a function <code>square_color</code> that tells if a chess 
+    square is black based off of its position (see the labeled board below).
 
-    <pl-code language="python">
-    >> is_sublist(['a', 'b', 'c', 'd'], ['b', 'c'])
-    True
-    >> is_sublist([1, 2, 3, 4], [4, 3])
-    False</pl-code>
+    <br>
+
+    <img src="https://www.dummies.com/wp-content/uploads/201843.image0.jpg" 
+        alt="chessboard" 
+        style="margin-left:auto; margin-right:auto; display:block; width:50;"
+    >
 """
 
 ## setup_code ##
 def to_coordinates(pos: str) -> tuple[int, int]:
-    """turns a file and rank string (ie 'c3') and turns it into an (int, int)"""
+    """ turns a file and rank string and turns it into an (int, int), 
+        eg 'a1' -> (0, 0), 'd6' -> (3, 5)"""
     f_ord, r_ord = tuple(map(ord, pos[:2]))
     return f_ord - ord('a'), r_ord - ord('1')
 ## setup_code ##
 
-def square_color(piece_position): #0given
-    file, rank = to_coordinates(piece_position)
-    return False #1given
+def square_color(pos): #0given
+    file, rank = to_coordinates(pos)
+    black_first_square = file % ?2? == 0
+    same_as_first = rank % ?2? == 0
+    return black_first_square == same_as_first
 
 ## test ##
 from pl_helpers import name, points
@@ -39,20 +43,16 @@ class Test(PLTestCase):
     @points(2)
     @name("example cases")
     def test_0(self):
-        score_cases(self.st.is_sublist, self.ref.is_sublist,
-            (['a', 'b', 'c', 'd'], ['b', 'c']),
-            ([1, 2, 3, 4], [4, 3])
+        score_cases(self.st.square_color, self.ref.square_color,
+            ('a1',),
+            ('d6',)
         )
 
     
     @points(8)
     @name("advanced cases")
     def test_1(self):
-        score_cases(self.st.is_sublist, self.ref.is_sublist,
-            ([1, 2, 3, 4], [2, 3]),
-            ([1, 2, 3, 4], [3, 2]),
-            ([1, 2, 3, 4], []),
-            ([1, 2, 3, 4], [1, 2, 3, 4]),
-            ([1, 2, 3, 4], [1, 2, 3, 4, 5]),
+        score_cases(self.st.square_color, self.ref.square_color,
+            *((f + str(r),) for f in 'abcdefg' for r in range(1, 9))
         )
 ## test ##
