2a9467fe97242225ab4ec66c8b1722ce05ac7dca
Logan Caraco <lbc.dna@gmail.com>
2021-09-15 15:24:42 -0700

    updated from lbc-dev

--- questions/square_color/tests/setup_code.py
+++ questions/square_color/tests/setup_code.py
@@ -1,4 +1,4 @@
-def to_coordinates(pos: str):
-    """description"""
-    file, rank = pos
-    return ord(file) - ord('a'), ord(rank) - ord('1')
+def to_coordinates(pos: str) -> tuple[int, int]:
+    """turns a file and rank string (ie 'c3') and turns it into an (int, int)"""
+    f_ord, r_ord = tuple(map(ord, pos[:2]))
+    return f_ord - ord('a'), r_ord - ord('1')
