1254989bfb2f4c6668f0d6cd53cdd3a51aa6b82c
Logan Caraco <lbc.dna@gmail.com>
2021-09-16 15:35:21 -0700

    added firebase comment, not sure how to use

--- elements/pl-faded-parsons/parsons-logging.js
+++ elements/pl-faded-parsons/parsons-logging.js
@@ -1,3 +1,25 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-analytics.js";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD3RdPsfO50vjyFKD6wYseNvdaQZFRslZg",
+//   authDomain: "faded-parsons-logging.firebaseapp.com",
+//   projectId: "faded-parsons-logging",
+//   storageBucket: "faded-parsons-logging.appspot.com",
+//   messagingSenderId: "591608062092",
+//   appId: "1:591608062092:web:4b4e4963e6026136119ba9",
+//   measurementId: "G-NCF4CRWMHG"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
 (function() {
     var ParsonsLogger = function(widget) {
         this.widget = widget;
