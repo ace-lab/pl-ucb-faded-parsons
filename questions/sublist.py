21939132abdb9b75db91b0b61a12b036d19ff8e8
Logan Caraco <lbc.dna@gmail.com>
2021-09-20 09:51:35 -0700

    changed the import region example

--- questions/sublist.py
+++ questions/sublist.py
@@ -1,4 +1,12 @@
-## import sublist_question.html as question_text ##
+""" Make a function <code>is_sublist</code> that checks whether the first
+    argument contains the second as a sublist (including ordering), eg
+
+    <pl-code language="python">
+    >> is_sublist(['a', 'b', 'c', 'd'], ['b', 'c'])
+    True
+    >> is_sublist([1, 2, 3, 4], [4, 3])
+    False</pl-code>
+"""
 
 def is_sublist(list, sublist): #0given
     n, m = len(list), len(sublist) #1given
