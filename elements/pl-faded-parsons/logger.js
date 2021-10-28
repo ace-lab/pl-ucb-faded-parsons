3ee85e486ed3cc430d80f5f1a4ca2f20f7d626af
Logan Caraco <lbc.dna@gmail.com>
2021-10-28 15:14:36 -0700

    split into separate logging files

--- elements/pl-faded-parsons/logger.js
+++ elements/pl-faded-parsons/logger.js
@@ -147,6 +147,11 @@ class Logger {
         this.logEvent(e);
     }
     
+    onSubmit() {
+        this.logEvent({ type: 'submit' });
+        this.commit();
+    }
+    
     async commit() {
         try {
             const FStore = Firebase.Firestore;
@@ -179,53 +184,5 @@ class Logger {
         } catch (e) {
             alert("Error adding document:\n" + e.toString());
         }
-    }
-}
-
-class ParsonsLogger extends Logger {
-    constructor(widget) {
-        super();
-        this.widget = widget;
-    }
-
-    _map_reinit(e) {
-        e.lines = this.widget.modified_lines.map(line =>
-            ({ id: line.id, code: line.code, indent: line.indent })
-        );
-        return e;
-    }
-
-    map_resume(e) { return this._reinit_mapper(e); }
-
-    map_init(e) { return this._reinit_mapper(e); }
-
-    map_commit(data) {
-        data.solutionCode = this.widget.solutionCode().map(t => t.replaceAll('\n', ';'));
-        return data;
-    }
-    
-    onSubmit() {
-        this.logEvent({ type: 'submit' });
-        this.commit();
-    }
-    
-    onSortableUpdate(event, ui) {
-        if (event.type === 'reindent') {
-            this.logEvent(event);
-            return;
-        }
-        const lineId = ui.item[0].id;
-        const solLines = [];
-        for (const child of event.target.children) {
-            const lineId = child.id;
-            const modLine = this.widget.getLineById(lineId);
-            const indent = modLine && modLine.indent;
-            solLines.push({ id: lineId, indent: indent });
-        }
-        this.logEvent({ type: event.type, targetId: lineId, solutionLines: solLines });
-    }
-    
-    onTextUpdate(event, codeline) {
-        super.onTextUpdate(event, codeline.name, codeline.value);
     }
 }
