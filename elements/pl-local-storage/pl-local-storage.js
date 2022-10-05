c6423a33c9cba16d40f479352fcb7e13f9dc652e
Logan Caraco <lbc.dna@gmail.com>
2022-10-04 20:34:38 -0700

    some progress

--- elements/pl-local-storage/pl-local-storage.js
+++ elements/pl-local-storage/pl-local-storage.js
@@ -1,3 +1,33 @@
 $(function() {
-
+  window['PlLocalStorage'] ||= {
+    stores: {},
+    registerStore: function(uuid, selector) {
+      const query = $(selector);
+      if (!query.length) throw new Error(`No element exists for selector '${selector}' (attepmting to register to uuid ${uuid})`);
+      this.stores[uuid] = query;
+      return contents => {
+        query.val(
+          btoa(
+            // he.encode(
+              contents,
+            //   {
+            //     allowUnsafeSymbols: true, // HTML tags should be kept
+            //     useNamedReferences: true,
+            //   }
+            // )
+          )
+        );
+      }
+    },
+    getStore: function(uuid) {
+      if (uuid in this.stores) return this.stores[uuid];
+      throw new Error(`No pl-local-storage is regestered to uuid: ${uuid}`);
+    },
+    getVal: function(uuid) {
+      return this.getStore(uuid).val()
+    },
+    setVal: function(uuid, contents) {
+      return this.getStore(uuid).val(contents);
+    }
+  }
 })
--- elements/pl-local-storage/pl-local-storage.py
+++ elements/pl-local-storage/pl-local-storage.py
@@ -1,16 +1,21 @@
 import random
 import chevron
+import lxml.html as xml
+import prairielearn as pl
 
 
 def prepare(element_html, data):
-    data['params']['random_number'] = random.random()
+    element = xml.fragment_fromstring(element_html)
+    required = ['key']
+    optional = []
+    pl.check_attribs(element, required, optional)
+    data['params']['key'] = pl.get_string_attrib(element, 'key', None)
     return data
 
 
 def render(element_html, data):
     html_params = {
-        'number': data['params']['random_number'],
-        'image_url': data['options']['client_files_element_url'] + '/block_i.png'
+        'key': data['params']['key']
     }
     with open('pl-local-storage.mustache', 'r') as f:
         return chevron.render(f, html_params).strip()
