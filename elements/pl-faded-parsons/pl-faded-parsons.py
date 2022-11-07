77aa2f369e6c61d2ae64ca9c66066c6808757829
Logan Caraco <lbc.dna@gmail.com>
2022-11-07 15:49:56 -0800

    removing unused code

--- elements/pl-faded-parsons/pl-faded-parsons.py
+++ elements/pl-faded-parsons/pl-faded-parsons.py
@@ -1,19 +1,9 @@
-import code
 import prairielearn as pl
 import lxml.html as xml
-import random
 import chevron
 import os
 import base64
 import json
-
-QUESTION_CODE_FILE     = 'code_lines.py'
-SOLUTION_CODE_FILE    = 'solution.py'
-SOLUTION_NOTES_FILE   = 'solution_notes.md'
-
-def prepare(element_html, data):
-    data['params']['random_number'] = random.random()
-    return data
 
 
 #
@@ -31,6 +21,7 @@ def read_file_lines(data, filename, error_if_not_found=True):
         else:
             return False
 
+
 def get_answers_name(element_html):
     # use answers-name to namespace multiple pl-faded-parsons elements on a page
     element = xml.fragment_fromstring(element_html)
@@ -41,22 +32,19 @@ def get_answers_name(element_html):
         answers_name = ''
     return answers_name
 
+
 def get_student_code(element_html, data):
-    element = xml.fragment_fromstring(element_html)
     answers_name = get_answers_name(element_html)
     student_code = data['submitted_answers'].get(answers_name + 'student-parsons-solution', None)
     return student_code
 
+
 def base64_encode(s):
     return base64.b64encode(s.encode("ascii")).decode("ascii")
 
-# def render_markdown(text):
-#     html = markdown.markdown(text)  # module 'markdown' no longer supported
-#     return html
 
 def render_question_panel(element_html, data):
     """Render the panel that displays the question (from code_lines.py) and interaction boxes"""
-
     element = xml.fragment_fromstring(element_html)
     answers_name = get_answers_name(element_html)
     vertical_format = pl.get_string_attrib(element, "format", "") == "vertical"
@@ -70,8 +58,8 @@ def render_question_panel(element_html, data):
     solution_order_info = json.loads(data['submitted_answers']['parsons-solution-order']) if 'parsons-solution-order' in data['submitted_answers'] else []
 
     html_params = {
-        "answers_name": answers_name,        
-        "code_lines":  str(element.text),
+        "answers_name": answers_name,
+        "code_lines": str(element.text),
         "populate_info": populate_info,
         "student_order_info": student_order_info,
         "solution_order_info": solution_order_info,
@@ -80,32 +68,31 @@ def render_question_panel(element_html, data):
     def get_child_text_by_tag(element, tag: str) -> str:
         """get the innerHTML of the first child of `element` that has the tag `tag`
         default value is empty string"""
-        return next( 
-            (   elem.text 
-                for elem in element 
-                if elem.tag == tag  ), 
+        return next(
+            (   elem.text
+                for elem in element
+                if elem.tag == tag  ),
             ""
         )
-    
-    try:    
+
+    try:
         code_lines = get_child_text_by_tag(element, "code-lines")
         if code_lines == "":
-            html_params.update({ 
-                "code_lines" : read_file_lines(data, QUESTION_CODE_FILE) 
+            html_params.update({
+                "code_lines" : read_file_lines(data, 'code_lines.py')
             })
         else:
             html_params.update({
                 "code_lines" : code_lines
             })
-    except StopIteration as e: 
-        # catch the error thrown by `get_child_text_by_tag` if there's 
+    except StopIteration as e:
+        # catch the error thrown by `get_child_text_by_tag` if there's
         #   no <code-lines> element
-        # if we do, then we don't need to update code-lines, as that means 
+        # if we do, then we don't need to update code-lines, as that means
         #   this is a question without the <code-lines> sub-element
         pass
 
-    if vertical_format: 
-        
+    if vertical_format:
         pre_text = get_child_text_by_tag(element, "pre-text")
         post_text = get_child_text_by_tag(element, "post-text")
 
@@ -115,7 +102,7 @@ def render_question_panel(element_html, data):
             pre_text = pre_text[:-1]
         while post_text[0] == '\n':
             post_text = post_text[1:]
-            
+
         html_params.update({
             "vertical" : {
                 "pre_text" : pre_text,
@@ -143,16 +130,16 @@ def render_submission_panel(element_html, data):
 
 def render_answer_panel(element_html, data):
     """Show the instructor's reference solution"""
-    answers_name = get_answers_name(element_html)
-    code = data['submitted_answers'].get(answers_name + 'code-lines', None)
     html_params = {
         "solution_path": "tests/ans.py",
-        # "notes": render_markdown(read_file_lines(data, SOLUTION_NOTES_FILE, error_if_not_found=False))
-        # "notes": data,
     }
     with open('pl-faded-parsons-answer.mustache', 'r') as f:
         return chevron.render(f, html_params).strip()
 
+
+#
+# Main functions
+#
 def render(element_html, data):
     panel_type = data['panel']
     if panel_type == 'question':
@@ -164,6 +151,7 @@ def render(element_html, data):
     else:
         raise Exception(f'Invalid panel type: {panel_type}')
 
+
 def parse(element_html, data):
     """Parse student's submitted answer (HTML form submission)"""
     # make an XML fragment that can be passed around to other PL functions,
@@ -171,27 +159,27 @@ def parse(element_html, data):
     element = xml.fragment_fromstring(element_html)
 
     # `element` is now an XML data structure - see docs for LXML library at lxml.de
-    
+
     # only Python problems are allowed right now (lang MUST be "py")
     # lang = pl.get_string_attrib(element, 'language') # TODO: commenting is a stop gap for the pilot study, find a better solution
 
     file_name = pl.get_string_attrib(element, 'file-name', 'user_code.py')
 
-    _files = [
+    data['submitted_answers']['_files'] = [
         {
             "name": file_name,
             "contents": base64_encode(get_student_code(element_html, data))
         }
     ]
-    data['submitted_answers']['_files'] = []
-    data['submitted_answers']['_files'].extend(_files)
+
     # TBD do error checking here for other attribute values....
     # set data['format_errors']['elt'] to an error message indicating an error with the
     # contents/format of the HTML element named 'elt'
-
     return
 
+
 def grade(element_html, data):
-    """Grade the student's response; many strategies are possible..."""
-    #no need because externally graded
+    """ Grade the student's response; many strategies are possible, but none are necessary.
+        This is externally autograded by a custom library.
+    """
     pass
